834611a7863bf1efa9dfab6d19e50556784f6417
nkasimakis <nkasimakis@network-error>
2019-09-06 14:21:06 +0300

    This is a cabinetCut project

--- main.cpp
+++ main.cpp
@@ -29,24 +29,51 @@ int main()
     float melamineBackThickness;
     float topmiddleBoards;
     int topboards;
-    int doorNum;
+    float doorNum;
     float doorHeight;
     float doorWidth;
     float doorThinckness;
+    float selectedNo = -1;
     
-    printf("Φάρδος ντουλαπιού? \n ");
+     while(1)
+    
+    {
+         
+    printf("Φάρδος ντουλαπιού εκατοστά? \n ");
     scanf(" %f", &cabinetSize);
+   
     
-    printf("Πάχος Μελαμίνης Πλαινών: \n ");
+    
+    if
+    (cabinetSize<=19 || cabinetSize>=121)
+    
+    {
+      
+    printf("Δεν μπορεί να είναι μικρότερο του 20 η μεγαλύτερο του 120!!\n");
+    
+    }
+    
+    else
+    
+    {
+      
+    //printf("Δεν θα έπρεπε να είναι μεγαλύτερο του 120.\n");
+      
+    break;
+    
+    }
+    
+    }
+    printf("Πάχος Μελαμίνης Πλαινών mm: \n ");
     scanf(" %f", &boardThickness);
     
-    printf("Πάχος Μελαμίνης  Πλάτης: \n ");
+    printf("Πάχος Μελαμίνης  Πλάτης mm: \n ");
     scanf(" %f", &boardBackThinckness);
     
-    printf("Αριθμός Πορτών: \n");
-    scanf(" %d", &doorNum);
+    printf("Αριθμός Πορτών τμχ: \n");
+    scanf(" %f", &doorNum);
     
-    printf("Πάχος Πόρτας: \n ");
+    printf("Πάχος Πόρτας mm: \n ");
     scanf(" %f", &doorThinckness);
     
     melamineThickness = (" %f", boardThickness);
@@ -66,17 +93,27 @@ int main()
     printf( "Πλαινά : %.1f x %.1f = 1 \n", middleBoard,depthofMiddle);
     printf(" PVC: %.1f = 1\n", middleBoard);
     printf("\n");
+    
     printf("Μεσιανά: %.1f x %.1f = 2 \n", heightofSide,widthofSide);
     printf(" PVC: %.1f = 2 και %.1f = 1 \n", heightofSide,widthofSide);
     printf("\n");
+    
     printf("Πήχες Πάνω: %d x %.1f = 2 \n", topboards,middleBoard);
     printf(" PVC: %.1f = 1 \n", middleBoard);
     printf("\n");
-    printf("Μελαμίνη %.1f Χιλιοστά \n", melamineBackThickness);
-    printf(" 78 x %f = 1", cabinetSize);
-    printf("\n");
-    printf("Πόρτα πάχους %.1f διαστάσεις %.1f x %.1f = %d τμχ", doorThinckness,doorHeight, doorWidth, doorNum);
     
+    printf("Μελαμίνη %.1f mm  \n", melamineBackThickness);
+    printf("\n");
+    
+    printf(" 78 x %.1f = 1 \n", cabinetSize);
+    printf("\n");
+    
+    printf("Πόρτα πάχους %.1f mm \n\n Διαστάσεις: \n %.1f x %.1f = %.f τμχ \n", doorThinckness,doorHeight, doorWidth, doorNum);
+    printf(" PVC: %.1f = 2 και %.1f = 2 \n", doorHeight,doorWidth);
+    
+    printf("\n");
+    
+    printf("\n");
     
     return 0;
 }
